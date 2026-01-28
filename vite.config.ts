f2450243d64e3da89552dac4f8df5b4e340d92b2
David Pinheiro <davrodpin+github@gmail.com>
2026-01-28 12:26:31 -0800

    chore: package assets required to publish the extension

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: "docs/STORE.md", dest: "docs" },
+        { src: "images/darktorch.1.png", dest: "images" },
+        { src: "images/darktorch.2.png", dest: "images" },
+      ],
+    }),
+  ],
   server: {
     host: true,
     port: 5174,
